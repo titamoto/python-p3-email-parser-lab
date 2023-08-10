3035a5987ffdfb24eeb167170db9e980ec33b535
Aleksandr <titamoto@gmail.com>
2023-08-09 21:40:53 -0700

    complete lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,15 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+
+    def __init__(self, addresses):
+        self.addresses = addresses
+
+    def parse(self):
+        separator = re.compile(r'\s|,(?:\s)|,')
+        addresses_list = sorted(list(tuple(separator.split(self.addresses))))
+        emails = []
+        for address in addresses_list:
+            if re.fullmatch(r'^\D[a-zA-Z0-9\.]+@[a-zA-Z]+\.[a-z]+', address):
+                emails.append(address)
+        return emails
